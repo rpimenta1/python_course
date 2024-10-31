64f566acfa419cb82d51bf8876335fb714e9b696
Rafael Pimenta <rafaelpimentadm@gmail.com>
2024-10-31 16:35:31 -0300

    Moved files to 01_python folder

--- challenge.py
+++ challenge.py
@@ -1,6 +1,6 @@
 BONUS_CONSTANT = 1000
 name = input("Write your name: ")
-salary = float(input("Write your salary (only numbers): "))
+salary = float(input("Write your salary (only numbers allowed: "))
 bonus = float(input("Write your bonus (only numbers - example 1.5): "))
 
 bonus_value = BONUS_CONSTANT + salary * bonus
