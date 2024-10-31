b509392741a25c62ade6fc81c86d6e8f4b35c864
Rafael Pimenta <73267455+rpimenta1@users.noreply.github.com>
2024-10-31 16:56:50 -0300

    Delete 02_exerc.py

--- 02_exerc.py
+++
@@ -1,2 +0,0 @@
-# Crie um programa aonde o usuário digite dois valores e apareça a soma dos respectivos números.
-print(int(input("Digite o primeiro número: "))+int(input("Digite o segundo número: ")))
